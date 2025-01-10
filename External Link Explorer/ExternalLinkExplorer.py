2ab8c34d857e25bfbeebc16974d6a83e58ce5b7c
ATrickett <aarontrickett@yahoo.com>
2025-01-10 13:42:06 -0500

    Update ExternalLinkExplorer.py

--- External Link Explorer/ExternalLinkExplorer.py
+++ External Link Explorer/ExternalLinkExplorer.py
@@ -12,7 +12,6 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive"
 }
 
